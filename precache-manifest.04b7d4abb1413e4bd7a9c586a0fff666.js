0f5194f77b9f506ca68901067f9351f74daedcfe
azottzsemle <subaistvan1997@gmail.com>
2020-04-17 15:27:22 +0200

    Updates

---
+++ precache-manifest.04b7d4abb1413e4bd7a9c586a0fff666.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad1fdad24898e93ec89547349438cd3",
+    "url": "/xanga/index.html"
+  },
+  {
+    "revision": "ab18a1da66aa3434301b",
+    "url": "/xanga/static/css/2.9d1cca57.chunk.css"
+  },
+  {
+    "revision": "9071bd30f9a07710b973",
+    "url": "/xanga/static/css/main.28404d98.chunk.css"
+  },
+  {
+    "revision": "ab18a1da66aa3434301b",
+    "url": "/xanga/static/js/2.66534409.chunk.js"
+  },
+  {
+    "revision": "9071bd30f9a07710b973",
+    "url": "/xanga/static/js/main.005b7a4d.chunk.js"
+  },
+  {
+    "revision": "38c58fcce3ab1ce4ce5d",
+    "url": "/xanga/static/js/runtime~main.d8a32beb.js"
+  },
+  {
+    "revision": "45feac8c7dce62ea49d0b457d7bd7daf",
+    "url": "/xanga/static/media/37746035_1678075772315180_7569253979385757696_o2.45feac8c.jpg"
+  },
+  {
+    "revision": "486bb2d5fbf30846d1f56e7a22f0411c",
+    "url": "/xanga/static/media/Flaticon.486bb2d5.ttf"
+  },
+  {
+    "revision": "54907e6425421b8e2c242ab8b0d453b7",
+    "url": "/xanga/static/media/Flaticon.54907e64.eot"
+  },
+  {
+    "revision": "5e21eb7ae31c525d3003d14d50a1a025",
+    "url": "/xanga/static/media/Flaticon.5e21eb7a.svg"
+  },
+  {
+    "revision": "85d205f36ed0dad86dbffe7c3ad74e5f",
+    "url": "/xanga/static/media/Flaticon.85d205f3.woff"
+  },
+  {
+    "revision": "93f6ba4edeedb24020f1009aee099bc4",
+    "url": "/xanga/static/media/Flaticon.93f6ba4e.woff2"
+  },
+  {
+    "revision": "9aa0028ee8e0b7650e1fb9fe70d68136",
+    "url": "/xanga/static/media/Flaticon.9aa0028e.woff2"
+  },
+  {
+    "revision": "a899202e989fca15172ce68290b3d433",
+    "url": "/xanga/static/media/Flaticon.a899202e.svg"
+  },
+  {
+    "revision": "d52339bc16eeac4e5e3d8f25909f0836",
+    "url": "/xanga/static/media/Flaticon.d52339bc.eot"
+  },
+  {
+    "revision": "dfcc7e9daa974ddfacac72be36440e27",
+    "url": "/xanga/static/media/Flaticon.dfcc7e9d.woff"
+  },
+  {
+    "revision": "fa16570a5c117ed4406e4d5c82d6ac97",
+    "url": "/xanga/static/media/Flaticon.fa16570a.ttf"
+  },
+  {
+    "revision": "ef6797903dd2b6c93da5fe86fc8f251e",
+    "url": "/xanga/static/media/awardBg.ef679790.png"
+  },
+  {
+    "revision": "d299faa5a50213d466e15c68edb324e3",
+    "url": "/xanga/static/media/cola_logo.d299faa5.png"
+  },
+  {
+    "revision": "6535f9c2b59801ca5b2ee90fc531e229",
+    "url": "/xanga/static/media/debrecenMapAndMarkers.6535f9c2.png"
+  },
+  {
+    "revision": "7dc7722a1dd38703a6457e0a25126328",
+    "url": "/xanga/static/media/laurel.7dc7722a.png"
+  },
+  {
+    "revision": "5917b664a5af0d1f860fcb185b785b41",
+    "url": "/xanga/static/media/laurel_modi.5917b664.png"
+  },
+  {
+    "revision": "9dc3023b80e2a4ad6fb826e1683dac35",
+    "url": "/xanga/static/media/logoWhite.9dc3023b.png"
+  },
+  {
+    "revision": "aa1d6c41a2dd83b9f8b81bc59e6a55b9",
+    "url": "/xanga/static/media/map3.aa1d6c41.png"
+  },
+  {
+    "revision": "14c17f9b25b0ea677cd7c6577724e3a9",
+    "url": "/xanga/static/media/mapSubPage.14c17f9b.png"
+  },
+  {
+    "revision": "976153bad4720e1fffb25a17de2f7e1e",
+    "url": "/xanga/static/media/mobile_header_image2.976153ba.png"
+  },
+  {
+    "revision": "7d6e363b7a1d8929295c66e2c2607cba",
+    "url": "/xanga/static/media/ni_logo.7d6e363b.png"
+  },
+  {
+    "revision": "863b29bf3df44bea641ddf0529a26580",
+    "url": "/xanga/static/media/refBg.863b29bf.png"
+  },
+  {
+    "revision": "70fa16a9a99aaa0f76b448bd37ac5a94",
+    "url": "/xanga/static/media/rg_logo.70fa16a9.png"
+  },
+  {
+    "revision": "852666b4f05bef4b9dfc6548b6b74dd4",
+    "url": "/xanga/static/media/shaelfer_logo.852666b4.png"
+  },
+  {
+    "revision": "2d24286533785c63b2c1150a4bbd1bf6",
+    "url": "/xanga/static/media/smallMap.2d242865.png"
+  },
+  {
+    "revision": "35e6f64c0142f0da545545372a035076",
+    "url": "/xanga/static/media/subpageHeader.35e6f64c.png"
+  },
+  {
+    "revision": "45f7d31e1698057741b20e7c76d78dcd",
+    "url": "/xanga/static/media/workImage1.45f7d31e.png"
+  },
+  {
+    "revision": "5cd02d7a02ab6da34a2af92f1270f3e9",
+    "url": "/xanga/static/media/workImage2.5cd02d7a.png"
+  },
+  {
+    "revision": "5b544c0018b460a62e923bb19fa59a25",
+    "url": "/xanga/static/media/workImage3.5b544c00.png"
+  },
+  {
+    "revision": "f3ef6ee89f378f0d33ac5815bf8bf5d0",
+    "url": "/xanga/static/media/workImage5.f3ef6ee8.png"
+  }
+]);
